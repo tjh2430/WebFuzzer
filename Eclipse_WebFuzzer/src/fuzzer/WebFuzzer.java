563ffa303c9577e3a2f2675a1a6687c1453566a6
Ross-Kahn <ross.kahn@outlook.com>
2013-01-28 15:46:15 -0500

    Eclipse project for the fuzzer

---
+++ Eclipse_WebFuzzer/src/fuzzer/WebFuzzer.java
@@ -0,0 +1,19 @@
+package fuzzer;
+
+/**
+ * Web Fuzzer for team 'Denial of Service'
+ * 
+ * @author Eric Newman
+ * @author Ross Kahn
+ * @author Timothy Heard
+ */
+public class WebFuzzer
+{
+	/**
+	 * Default WebFuzzer constructor.
+	 */
+	public WebFuzzer()
+	{
+		
+	}
+}
