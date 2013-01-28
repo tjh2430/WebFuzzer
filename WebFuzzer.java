f0eb24b8af5406c36723614fd0bbc17fde2d372c
tjh2430 <timheard@hotmail.com>
2013-01-28 15:33:42 -0500

    Added HTML Unit files to the repo and filled WebFuzzer.java in with a class stub.

---
+++ WebFuzzer.java
@@ -0,0 +1,18 @@
+
+/**
+ * Web Fuzzer for team 'Denial of Service'
+ * 
+ * @author Eric Newman
+ * @author Ross Kahn
+ * @author Timothy Heard
+ */
+public class WebFuzzer
+{
+	/**
+	 * Default WebFuzzer constructor.
+	 */
+	public WebFuzzer()
+	{
+		
+	}
+}
