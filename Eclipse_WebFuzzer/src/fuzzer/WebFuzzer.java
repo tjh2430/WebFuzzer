63520c623ba97c9f342dcdbb61d0d0d04fc9d9a9
Eric Newman <edn6266@g.rit.edu>
2013-02-01 12:38:43 -0500

    Initial commit of submission web fuzzer code

--- Eclipse_WebFuzzer/src/fuzzer/WebFuzzer.java
+++ Eclipse_WebFuzzer/src/fuzzer/WebFuzzer.java
@@ -1,8 +1,18 @@
 package fuzzer;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 /**
  * Web Fuzzer for team 'Denial of Service'
  * 
@@ -12,15 +22,78 @@ import java.util.ArrayList;
  */
 public class WebFuzzer
 {
-	private final URL rootURL;
-	private ArrayList<URL> links;
 	
 	/**
 	 * Default WebFuzzer constructor.
 	 */
-	public WebFuzzer(URL url)
+	public WebFuzzer( URL url )
 	{
-		rootURL = url;
-		links = new ArrayList<URL>(); 
+		
+	}
+	
+	/*
+	 * Method to start crawling the page
+	 */
+	public static void fuzz(String url, Set<String> previousUrls) throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{
+		WebClient client = new WebClient();
+		HtmlPage page = client.getPage(url);
+		DomNodeList<DomElement> inputs = page.getElementsByTagName("input");
+		
+		System.out.printf("\"%s\" Inputs:\n", url);
+		for(DomElement e: inputs){
+			System.out.println(e.asText());
+		}
+		
+		// Print an extra new line to improve formatting
+		System.out.println();
+		
+		List<HtmlAnchor> links = getLinks(url);
+		for(HtmlAnchor link: links)
+		{
+			String linkUrl = page.getFullyQualifiedUrl(link.getHrefAttribute()).toString();
+			if(previousUrls.contains(linkUrl))
+			{
+				continue;
+			}
+						
+			previousUrls.add(linkUrl);
+			fuzz(linkUrl, previousUrls);			
+		}
+	}
+	
+	public static List<HtmlAnchor> getLinks(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{
+		WebClient client = new WebClient();
+		HtmlPage page = client.getPage(url);
+		
+		return page.getAnchors();
+	}
+	
+	/*
+	 * Main method to run from command line.
+	 */
+	public static void main (String[] args)
+	{
+		if(args.length != 1)
+		{
+			System.out.println("Usage: java WebFuzzer <url>");
+			return;
+		}
+		
+		try {
+			fuzz(args[0], new HashSet<String>());			
+		} catch (FailingHttpStatusCodeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
