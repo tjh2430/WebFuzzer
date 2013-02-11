cbf47d3e81c365f4d1ce164617dfc5781cf2382e
Ross-Kahn <ross.kahn@outlook.com>
2013-02-10 19:59:26 -0500

    Most of the command line interface. Needs to be fixed to work with the new fuzzer classes (using the old static one right now)

--- Eclipse_WebFuzzer/src/ui/CLI.java
+++ Eclipse_WebFuzzer/src/ui/CLI.java
@@ -4,11 +4,15 @@ import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import fuzzer.FuzzerData;
 import fuzzer.WebFuzzer;
 
 
 
 public class CLI {
+	
+	private static final boolean RUN = true;
+	private static final boolean EXIT = false;
 
 	private boolean startUp(){
 		System.out.println("=========================");
@@ -22,28 +26,23 @@ public class CLI {
 		
 		System.out.print(": ");
 		
-		Scanner scanner = new Scanner(System.in);
-		try{
-			int response = scanner.nextInt();
+		int response = getMenuSelection();
+		
+		switch(response){
+		
+		case -1:
+			return startUp();
+		
+		case 1:
+			return fuzz();
+		
+		case 2: 
+			return options();
 			
-			switch(response){
-			
-			case 1:
-				return fuzz();
-			
-			case 2: 
-				return options();
-				
-			case 3:
-				return false;
-			}
-			
-			
-		}catch(InputMismatchException ime){
-			System.err.println("Error: please enter the number corresponding to the option");
-		}catch(Exception e){
-			e.printStackTrace();
+		case 3:
+			return EXIT;
 		}
+		
 		
 		return true;
 	}
@@ -52,37 +51,67 @@ public class CLI {
 		System.out.println("\nSystem Options:");
 		System.out.println("\t1. Set Time Gaps");
 		System.out.println("\t2. Set Completeness");
+		System.out.println("\t3. Show Current Settings");
+		System.out.println("\t4. <-- Main Menu");
+		System.out.print(": ");
 		
-		Scanner scanner = new Scanner(System.in);
-		try{
-			int response = scanner.nextInt();
-			
-			switch(response){
+		boolean validResponse = true;		
 		
-			case 1:
-				return true;
-				
-			case 2:
-				return true;
+		int response = getMenuSelection();
+		switch(response){
+		
+		case -1:
+			return options();
+		
+		case 1:
+			validResponse = setTimeGap();
+			while(!validResponse){
+				validResponse = setTimeGap();
 			}
-	
-		}catch(InputMismatchException ime){
-			System.err.println("Error: please enter the number corresponding to the option");
-		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println("\n--------------------\n" + 
+					FuzzerData.getInstance().toString() + 
+					"--------------------");
+			return options();
+			
+		case 2:
+			validResponse = setCompleteness();
+			while(!validResponse){
+				validResponse = setCompleteness();
+			}
+			System.out.println("\n--------------------\n" + 
+					FuzzerData.getInstance().toString() + 
+					"--------------------");
+			return options();
+			
+		case 3:
+			System.out.println("\n--------------------\n" + 
+								FuzzerData.getInstance().toString() + 
+								"--------------------");
+			return options();
+			
+		case 4:
+			return RUN;
+			
+		default:
+			return options();
+			
 		}
-		
-		return true;
 	}
 	
 	private boolean fuzz(){
 		
 		System.out.println("\n--- Fuzz a Web Page --- ");
+		System.out.println("Type 'BACK' to go back to the main menu");
 		System.out.print("Enter a URL: ");
 		
 		Scanner scanner = new Scanner(System.in);
 		try{
 			String response = scanner.next();
+			
+			if (response.toLowerCase().equals("BACK")){
+				return RUN;
+			}
+			
 			
 			WebFuzzer.fuzz(WebFuzzer.getBaseUrl(response), response, new HashSet<String>());
 			
@@ -92,7 +121,75 @@ public class CLI {
 			e.printStackTrace();
 		}
 		
-		return false;
+		return RUN;
+	}
+	
+	private boolean setTimeGap(){
+		System.out.println("\n--- Set Time Gap ---");
+		System.out.println("Type -1 to go back to the main menu");
+		System.out.print("Please enter the time gap in milliseconds: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		int gap = 0;
+		try{
+			gap = scanner.nextInt();
+			if(gap < 0){
+				return true;
+			}
+		}catch(InputMismatchException ime){
+			System.out.println("\nERROR: please enter a time gap number in milliseconds\n");
+			return false;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		
+		FuzzerData.getInstance().setTimeGap(gap);
+		return true;
+	}
+	
+	private boolean setCompleteness(){
+		System.out.println("\n--- Set Completeness ---:");
+		System.out.println("\t1. Complete discovery");
+		System.out.println("\t2. Random discovery");
+		System.out.println("\t3. <-- Main Menu");
+		System.out.print(": ");
+		
+		int response = getMenuSelection();
+		
+		switch(response){
+		case 1:
+			FuzzerData.getInstance().setCompleteness(true);
+			return true;
+			
+		case 2:
+			FuzzerData.getInstance().setCompleteness(false);
+			return true;
+			
+		case 3:
+			return true;
+			
+		default:
+			System.out.println("Not a valid option");
+			return false;
+		}
+	}
+	
+	private int getMenuSelection(){
+		Scanner scanner = new Scanner(System.in);
+		int response;
+		try{
+			response = scanner.nextInt();
+			
+		}catch(InputMismatchException ime){
+			System.err.println("Error: please enter the number corresponding to the option");
+			response = -1;
+		}catch(Exception e){
+			e.printStackTrace();
+			response = -1;
+		}
+		
+		return response;
 	}
 	
 	/**
@@ -105,7 +202,6 @@ public class CLI {
 			System.out.println("Usage: java WebFuzzer <url>");
 			return;
 		}*/
-		
 		CLI cli = new CLI();
 		
 		boolean runFlag = true;
